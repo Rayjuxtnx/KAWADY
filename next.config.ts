341db0929c4dc82e9c18cf6dc0974b3a9b390b6d
Philip Onyango <onyangophilip244@gmail.com>
2025-10-31 16:01:51 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,19 @@
 
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
-
-const config = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
 };
 
-const withPWAConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development' ? false : true,
-});
-
-export default withPWAConfig(config);
+export default nextConfig;
