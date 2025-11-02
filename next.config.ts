70e90caefc8a8e803392cf12ca0c1ec7ceef2a72
Philip Onyango <onyangophilip244@gmail.com>
2025-11-02 18:30:32 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+        'https://6000-firebase-studio-1761747951618.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev'
+    ]
+  },
 };
 
 export default nextConfig;
