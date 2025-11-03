b52382c33977a091a01c5c2173d6e2513da389d6
philip otieno <onyangophilip244@gmail.com>
2025-11-03 10:54:04 +0000

    IMG

--- src/lib/gallery-data.ts
+++ src/lib/gallery-data.ts
@@ -48,5 +48,97 @@ export const galleryImages: GalleryItem[] = [
     "id": "gallery-10",
     "title": "Project C",
     "imageId": "gallery-img-10"
-  }
-];
+  },
+  { "id": "gallery-11",
+    "title": "Project D",
+    "imageId": "gallery-img-11"
+  },
+  { "id": "gallery-12",
+    "title": "On site",
+    "imageId": "gallery-img-12"
+  },
+  { "id": "gallery-13",
+    "title": "Project E",
+    "imageId": "gallery-img-13"
+  },
+  { "id": "gallery-14",
+    "title": "Finished Project",
+    "imageId": "gallery-img-14"
+  },
+  { "id": "gallery-15",
+    "title": "Project F",
+    "imageId": "gallery-img-15"
+  },
+  { "id": "gallery-16",
+    "title": "Project G",
+    "imageId": "gallery-img-16"
+  },
+  { "id": "gallery-17",
+    "title": "Project H",
+    "imageId": "gallery-img-17"
+  },
+  { "id": "gallery-18",
+    "title": "Project I",
+    "imageId": "gallery-img-18"
+  },
+  { "id": "gallery-19",
+    "title": "Project J",
+    "imageId": "gallery-img-19"
+  },
+  { "id": "gallery-20",
+    "title": "Project K",
+    "imageId": "gallery-img-20"
+  },
+  { "id": "gallery-21",
+    "title": "Project L",
+    "imageId": "gallery-img-21"
+  },
+  { "id": "gallery-22",
+    "title": "Project M",
+    "imageId": "gallery-img-22"
+  },
+  { "id": "gallery-23",
+    "title": "Project N",
+    "imageId": "gallery-img-23"
+  },
+  { "id": "gallery-24",
+    "title": "Project O",
+    "imageId": "gallery-img-24"
+  },
+  { "id": "gallery-25",
+    "title": "Project P",
+    "imageId": "gallery-img-25"
+  },
+  { "id": "gallery-26",
+    "title": "Project Q",
+    "imageId": "gallery-img-26"
+  },
+  { "id": "gallery-27",
+    "title": "Project R",
+    "imageId": "gallery-img-27"
+  },
+  { "id": "gallery-28",
+    "title": "Project S",
+    "imageId": "gallery-img-28"
+  },
+  { "id": "gallery-1",
+    "title": "Project D",
+    "imageId": "gallery-img"
+  },
+  { "id": "gallery-11",
+    "title": "Project D",
+    "imageId": "gallery-img"
+  },
+  { "id": "gallery-11",
+    "title": "Project D",
+    "imageId": "gallery-img"
+  },
+  { "id": "gallery-11",
+    "title": "Project D",
+    "imageId": "gallery-img"
+  },
+  { "id": "gallery-11",
+    "title": "Project D",
+    "imageId": "gallery-img"
+  }                                              
+]
